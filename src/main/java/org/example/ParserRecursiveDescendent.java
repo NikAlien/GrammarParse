0688091f67dd4a9c57f791c1c81fb1176c8a25a5
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-11-29 14:55:09 +0200

    Skeleton of class

---
+++ src/main/java/org/example/ParserRecursiveDescendent.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class ParserRecursiveDescendent {
+    //TODO: Tests!!!!
+    private Grammar grammar;
+
+    public ParserRecursiveDescendent(Grammar grammar) {
+        this.grammar = grammar;
+    }
+
+    //TODO algorithm course 6
+    public void  recursiveDescendentParsing(String w) {
+    }
+
+    public void Expand() {
+
+    }
+
+    public void Advance() {
+
+    }
+
+    public void MomentaryInsuccess() {
+
+    }
+
+    public void Back() {
+
+    }
+
+    public void AnotherTry() {
+
+    }
+
+    public void Success() {
+
+    }
+}
